1f40d8f15e9ff4e587147e0dd2911b73a93ac65f
João Cotrim <140359552+Noitah@users.noreply.github.com>
2026-05-10 18:53:13 -0300

    Delete executor_gui_english directory

--- executor_gui_english/src/__init__.py
+++
@@ -1,2 +0,0 @@
-__version__ = "7.0.0"
-__author__ = "João Victor Cotrim"
--- executor_gui_english/src/core/__init__.py
+++
@@ -1,5 +0,0 @@
-from .validators import Validators
-from .file_handler import FileHandler
-from .module_loader import ModuleLoader
-
-__all__ = ['Validators', 'FileHandler', 'ModuleLoader']
