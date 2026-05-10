0ef066b105880ec10709385db51025fb634e75ea
João Cotrim <140359552+Noitah@users.noreply.github.com>
2026-05-10 18:51:43 -0300

    Delete executor_gui_english/executor_gui_english directory

--- executor_gui_english/executor_gui_english/src/ui/__init__.py
+++
@@ -1,5 +0,0 @@
-from .widgets import DescriptionLabel, LogFrame, StatusBar, FileField
-from .tabs import TabFactory
-from .main_window import ExecutorGUI
-
-__all__ = ['DescriptionLabel', 'LogFrame', 'StatusBar', 'FileField', 'TabFactory', 'ExecutorGUI']
